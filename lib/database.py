c88c6d8882818ea47c3171d8ad7a9ef3ab5af4c0
agbassist <agbassist@gmail.com>
2021-02-02 21:39:45 -0600

    Add database support for creating a shopping list
    (and some cleanup)

--- lib/database.py
+++ lib/database.py
@@ -3,29 +3,42 @@ from sqlite3 import Error
 
 db_file = 'database/instacart.db'
 
-create_ingredients_table = """CREATE TABLE IF NOT EXISTS ingredients (
-                                    id        integer  NOT NULL PRIMARY KEY,
-                                    name      text     NOT NULL,
-                                    search    text,
-                                    quantity  integer,
-                                    unit      text
+sql_create_ingredients_table = """CREATE TABLE IF NOT EXISTS ingredients (
+                                  id        integer  NOT NULL PRIMARY KEY,
+                                  name      text     NOT NULL,
+                                  search    text,
+                                  quantity  integer,
+                                  unit      text
+                               );"""
+
+sql_create_recipes_table = """CREATE TABLE IF NOT EXISTS recipes (
+                              id    integer  NOT NULL PRIMARY KEY,
+                              name  text     NOT NULL
+                           );"""
+
+sql_create_recipe_items_table = """CREATE TABLE IF NOT EXISTS recipe_items (
+                                   id          integer  NOT NULL PRIMARY KEY,
+                                   recipe      integer  NOT NULL,
+                                   ingredient  integer  NOT NULL,
+                                   quantity    integer,
+                                   unit        text,
+                                   FOREIGN KEY (recipe) references recipe (id),
+                                   FOREIGN KEY (ingredient) references ingredients (id)
                                 );"""
 
-create_recipes_table = """CREATE TABLE IF NOT EXISTS recipes (
-                                    id    integer  NOT NULL PRIMARY KEY,
-                                    name  text     NOT NULL
-                                );"""
+sql_create_selected_recipes_table = """CREATE TABLE IF NOT EXISTS selected_recipes (
+                                       id        integer  NOT NULL PRIMARY KEY,
+                                       recipe    integer  NOT NULL,
+                                       FOREIGN KEY (recipe) references recipe (id)
+                                    );"""
 
-create_recipe_items_table = """CREATE TABLE IF NOT EXISTS recipe_items (
-                                    id          integer  NOT NULL PRIMARY KEY,
-                                    recipe      integer  NOT NULL,
-                                    ingredient  integer  NOT NULL,
-                                    quantity    integer,
-                                    unit        text,
-                                    FOREIGN KEY (recipe) references recipe (id),
-                                    FOREIGN KEY (ingredient) references ingredients (id)
-                                );"""
-
+sql_create_selected_ingredients_table = """CREATE TABLE IF NOT EXISTS selected_recipes (
+                                           id         integer  NOT NULL PRIMARY KEY,
+                                           ingredient integer  NOT NULL,
+                                           quantity   integer,
+                                           unit       text,
+                                           FOREIGN KEY (ingredient) references ingredients (id)
+                                        );"""                                
 
 class Database:
 
@@ -62,9 +75,12 @@ class Database:
     #                     Ingredients
     #######################################################
 
+    def create_ingredient_table( self ):
+        self.execute( sql_create_ingredients_table )
+
     def add_ingredient( self, name, search, quantity, unit ):
-        sql = ''' INSERT INTO ingredients(name,search,quantity,unit)
-                  VALUES(?,?,?,?) '''
+        sql = ''' INSERT INTO ingredients( name, search, quantity, unit )
+                  VALUES( ?, ?, ?, ? ) '''
         self.execute( sql )
 
     def get_all_ingredients( self ):
@@ -84,10 +100,13 @@ class Database:
     #                       Recipes
     #######################################################
 
+    def create_recipes_table( self ):
+        self.execute( sql_create_recipes_table )
+
     def add_recipe( self, name ):
-        sql = ''' INSERT INTO recipes(name)
-                  VALUES(?) '''
-        self.execute( sql, (name,) )
+        sql = ''' INSERT INTO recipes( name )
+                  VALUES( ? ) '''
+        self.execute( sql, ( name, ) )
 
     def get_all_recipes( self ):
         return self.fetch( 'SELECT * FROM recipes' )
@@ -96,17 +115,24 @@ class Database:
         ret = self.fetch( 'SELECT id FROM recipes WHERE name="{}"'.format( recipe ) )
         return ret[0][0]
 
+    def get_recipe_name( self, id ):
+        ret = self.fetch( 'SELECT name FROM recipes WHERE id="{}"'.format( id ) )
+        return ret[0][0]
+
     #######################################################
     #                    Recipe Items
     #######################################################
+
+    def create_recipe_items_table( self ):
+        self.execute( sql_create_recipe_items_table )
 
     def delete_recipe_item_by_id( self, id ):
         self.execute( 'DELETE FROM recipe_items WHERE id={}'.format( id ) )
 
     def add_recipe_item( self, recipe, ingredient, quantity, unit ):
-        sql = ''' INSERT INTO recipe_items(recipe,ingredient,quantity,unit)
-                  VALUES(?,?,?,?) '''
-        self.execute( sql, (recipe,ingredient,quantity,unit) )
+        sql = ''' INSERT INTO recipe_items( recipe, ingredient, quantity, unit)
+                  VALUES( ?, ?, ?, ? ) '''
+        self.execute( sql, ( recipe, ingredient, quantity, unit ) )
 
     def get_items_for_recipe( self, recipe_id ):
         sql = ''' SELECT
@@ -123,8 +149,39 @@ class Database:
 
         return self.fetch( sql )
 
+    #######################################################
+    #                  Selected Recipes
+    #######################################################
+
+    def create_selected_recipes_table( self ):
+        self.execute( sql_create_selected_recipes_table )
+
+    def add_selected_recipe( self, recipe_id ):
+        sql = ''' INSERT INTO selected_recipes( id )
+                  VALUES( ? ) '''
+        self.execute( sql, ( recipe_id, ) )
+
+    def remove_selected_recipe( self, id ):
+        self.execute( 'DELETE FROM selected_recipes WHERE id={}'.format( id ) )
+
+    #######################################################
+    #                Selected Ingredients
+    #######################################################
+
+    def create_selected_ingredients_table( self ):
+        self.execute( sql_create_selected_ingredients_table )
+
+    def add_selected_ingredient( self, ingredient_id, quantity, unit ):
+        sql = ''' INSERT INTO selected_ingredients( id, quantity, unit )
+                  VALUES( ?, ?, ? ) '''
+        self.execute( sql, ( ingredient_id, quantity, unit ) )
+
+    def remove_selected_ingredient( self, id ):
+        self.execute( 'DELETE FROM selected_ingredients WHERE id={}'.format( id ) )
+
 # Temporary stuff and testing
 if __name__ == '__main__':
 
     database = Database()
-    database.execute( 'DROP TABLE temp;' )
+    database.create_selected_recipes_table()
+    database.create_selected_ingredients_table()
