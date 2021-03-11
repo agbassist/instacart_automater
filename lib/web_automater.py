79b6286c398125a5baa2c627328d6e7d6f10ddc8
agbassist <agbassist@gmail.com>
2021-03-10 19:25:02 -0600

    Improvements to web_automater on slow connection

--- lib/web_automater.py
+++ lib/web_automater.py
@@ -1,5 +1,9 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 from time import sleep
 from lib.ingredient import Ingredient
 import json
@@ -13,7 +17,7 @@ def search_for_item( browser, item_string, quantity ):
         item_string.replace( r'%', r'%25' ).replace( ',', r'%2C' ).replace( ' ', r'%20' ) ) )
 
     browser.find_element_by_xpath(
-        '//button[@aria-label="Add 1 unit of {}"]'.format( item_string ) ).click()
+        '//button[@aria-label="Add 1 item {}"]'.format( item_string ) ).click()
 
     for i in range( 1, quantity ):
         browser.find_element_by_xpath(
@@ -28,6 +32,9 @@ def build_cart( shopping_list ):
     browser = Chrome( options=opts )
     browser.implicitly_wait( 30 )
     browser.get( 'https://www.instacart.com/store/home' )
+
+    # Set explicit wait time
+    wait = WebDriverWait( browser, 60 )
 
     # Log in
     browser.find_element_by_xpath( '//button[text()="Log in"]' ).click()
@@ -50,13 +57,16 @@ def build_cart( shopping_list ):
         '//span[text()="Next"]/parent::button' ).click()
 
     # Enter Password
-    password_input = browser.find_element_by_xpath(
-        '//input[@type="password"]' )
+    password_input = wait.until( EC.element_to_be_clickable( ( By.XPATH, '//input[@type="password"]' ) ) )
     password_input.send_keys( password )
 
     browser.find_element_by_xpath(
         '//span[text()="Next"]/parent::button' ).click()
 
+    # Wait for the page to load to complete login
+    wait.until( EC.element_to_be_clickable( ( By.XPATH, '//button[@data-identifier="cart_view_button"]' ) ) )
+
+    # Go to the ALDI storefront page
     browser.get( 'https://www.instacart.com/store/aldi/storefront' )
 
     # Clear the cart if it is filled
